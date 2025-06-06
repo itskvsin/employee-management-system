6e7b81657d8880f4cd351d8716d15cae17e1915f
Kevin <kevinsolankistudy@gmail.com>
2025-06-06 15:48:12 +0530

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return(
+    <div>
+      <h1 className="bg-gray-500 p-6 text-2xl">This is a employee management system react project</h1>
+    </div>
+  )
+}
+
+export default App;
